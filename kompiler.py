f3dda05566f17730a34ec5433000987ac8e462d2
S1mon <jaforsi.kozak@gmail.com>
2026-08-22 06:02:22 +0200

    implementierte zerteiler

--- kompiler.py
+++ kompiler.py
@@ -1,5 +1,6 @@
 import sys
 from dataclasses import dataclass as dk #datenklass
+from typing import Any
 
 
 def ZeichenEinOrdne(zeichen):
@@ -7,6 +8,7 @@ def ZeichenEinOrdne(zeichen):
         case x if x.isdigit(): return "zahl"
         case x if x.isalpha(): return "wort"
         case '_':              return "wort"
+        case ':':              return "wort"
 
         #Klammern müssen immer direkt
         # als Marken abgegeben werden. 
@@ -31,10 +33,13 @@ class Marke:
 @dk
 class Fluss:
     marken : list[Marke]
-    index  : int
+    index  : int = 0
+
+    def schau_marke(self):
+        return self.marken[self.index]
 
     def schau(self):
-        return self.marken[self.index]
+        return self.schau_marke().inhalt
 
     def nimm(self):
         marke = self.schau()
@@ -44,47 +49,268 @@ class Fluss:
     def hat(self):
         return self.index < len(self.marken)
 
+    def erwarte(self, soll):
+        ist = self.schau_marke()
+        if ist.inhalt != soll:
+            print(f"Syntaxfehler in Zeile {ist.zeile}: Erwarte `{soll}`, aber habe `{ist.inhalt}` bekomment.")
+            fuck()
+            sys.exit(1)
+        self.nimm()
 
 def LexAnalyse(pfad):
     with open(pfad, 'r', encoding='utf-8') as f:
         quelle = f.read()
 
-    puffer = ""
-    zeile = 0
+    puffer = []
+    zeile = 1
     letzter_zustand = None
+    kontrol_zeichen = False
 
     fluss = []
     for zeichen in quelle:
         dieser_zustand = ZeichenEinOrdne(zeichen)
+        
+        if zeichen == '\n':
+            zeile += 1
+
+        if kontrol_zeichen:
+            puffer.pop(-1)
+            match zeichen:
+                case '0': puffer.append('\0')
+            kontrol_zeichen = False
+            continue
 
         if letzter_zustand and dieser_zustand != letzter_zustand:
             if letzter_zustand != "formatierung":
-                fluss.append(Marke(puffer, zeile))
+                fluss.append(Marke("".join(puffer), zeile))
 
-            puffer = ""
+            puffer = []
 
-        puffer += zeichen
+        puffer.append(zeichen)
         letzter_zustand = dieser_zustand
+        kontrol_zeichen = (zeichen == '\\')
 
-    return fluss
+    return Fluss(fluss)
 
+
+@dk
+class AsbAufruf:
+    name : str
+    parameter : Any
+
+    @classmethod
+    def zerteil(kls, fluss, name):
+        fluss.erwarte("(")
+
+        parameter = []
+        while fluss.schau() != ')':
+            parameter.append(AsbBinär.zerteil(fluss))
+            if fluss.schau() == ',':
+                fluss.nimm()
+
+        fluss.erwarte(")")
+        return kls(name, parameter)
+
+
+
+
+
+OPERATOREN = ['+', '-', '*', '/', '.', '<<', '>>', '&', '|', '^', '==', '!=', '>', '<']
+
+@dk
+class AsbUnär:
+    art : str
+    inhalt : Any
+
+    @classmethod
+    def zerteil(kls, fluss):
+        match fluss.nimm():
+            case zahl if zahl.isdigit():
+                art = "zahl"
+                inhalt = int(zahl)
+            case '(':
+                unterausdruck = AsbBinär.zerteil(fluss)
+                fluss.erwarte(')')
+                return unterausdruck
+
+            case '›':
+                art = "zeichen"
+                inhalt = fluss.nimm()
+                fluss.erwarte('‹')
+
+            case '-':
+                art = "minus"
+                inhalt = AsbUnär.zerteil(fluss)
+
+            case name if fluss.schau() == '(':
+                art = "aufruf"
+                inhalt = AsbAufruf.zerteil(fluss, name)
+
+            case wort:
+                art = "zugriff"
+                inhalt = wort
+
+
+        return kls(art, inhalt)
+
+
+
+
+@dk
+class AsbBinär:
+    links  : Any
+    rechts : Any
+    operator : str
+
+    @classmethod
+    def zerteil(kls, fluss):
+        links = AsbUnär.zerteil(fluss)
+        operator = fluss.schau()
+
+        if operator not in OPERATOREN:
+            return links
+
+        fluss.nimm()
+        rechts = AsbBinär.zerteil(fluss)
+        return kls(links, rechts, operator)
+
+@dk
+class AsbTu:
+    variable : str
+    quelle   : AsbBinär
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte("tu")
+        variable = fluss.nimm()
+        fluss.erwarte("=")
+        quelle = AsbBinär.zerteil(fluss)
+        fluss.erwarte(";")
+        return kls(variable, quelle)
+
+
+@dk
+class AsbRück:
+    ziel : AsbBinär
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte("rück")
+        ziel = AsbBinär.zerteil(fluss)
+        fluss.erwarte(";")
+        return kls(ziel)
+
+@dk
+class AsbSolang: 
+    bedingung : AsbBinär
+    körper    : "AsbAbschnitt"
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte("solang")
+        bedingung = AsbBinär.zerteil(fluss)
+        körper    = AsbAbschnitt.zerteil(fluss)
+        return kls(bedingung, körper)
+
+@dk
+class AsbFalls: 
+    bedingung : AsbBinär
+    körper    : "AsbAbschnitt"
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte("falls")
+        bedingung = AsbBinär.zerteil(fluss)
+        körper    = AsbAbschnitt(fluss)
+        return kls(bedingung, körper)
+
+@dk
+class AsbAusdruck: 
+    ziel : AsbBinär
+
+    @classmethod
+    def zerteil(kls, fluss):
+        ziel = AsbBinär.zerteil(fluss)
+        fluss.erwarte(";")
+        return kls(ziel)
+
+
+@dk
+class AsbAussage:
+    @classmethod
+    def zerteil(kls, fluss):
+        match fluss.schau():
+            case 'tu'    : return AsbTu.zerteil(fluss)
+            case 'rück'  : return AsbRück.zerteil(fluss)
+            case 'solang': return AsbSolang.zerteil(fluss)
+            case 'falls' : return AsbFalls.zerteil(fluss)
+            case _       : return AsbAusdruck.zerteil(fluss)
+
+
+@dk
+class AsbAbschnitt:
+    aussagen: list[Any]
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte("auf")
+
+        aussagen = []
+        while fluss.schau() != "zu":
+            aussagen.append(AsbAussage.zerteil(fluss))
+
+        fluss.erwarte("zu")
+        return kls(aussagen)
+
+
+@dk
+class AsbProzedur:
+    name : str
+    parameter : list[str]
+    körper : AsbAbschnitt
+
+    @classmethod
+    def zerteil(kls, fluss):
+        fluss.erwarte('prozedur')
+        name = fluss.nimm()
+        fluss.erwarte('(')
+
+        parameter = []
+        while fluss.schau() != ')':
+            parameter.append(fluss.nimm())
+            if fluss.schau() == ',':
+                fluss.nimm()
+
+        fluss.erwarte(')')
+        körper = AsbAbschnitt.zerteil(fluss)
+        return kls(name, parameter, körper)
+
+
+@dk
 class AsbProgramm:
     prodezuren : list[AsbProzedur]
     konstantent : dict[str, int]
 
+    @classmethod
     def zerteil(kls, fluss):
-        prodezuren  = []
+        prozeduren  = []
         konstantent = {}
 
         while fluss.hat():
             match fluss.schau():
-                case 'prozedur':  prodezuren.append(AsbProzedur.zerteil(fluss))
+                case 'prozedur':  
+                    prozeduren.append(AsbProzedur.zerteil(fluss))
                 case 'konstant':  
+                    fluss.nimm()
                     name = fluss.nimm()
                     fluss.erwarte("=")
-                    wert = int(fluss.nimm())
+                    wert = AsbBinär.zerteil(fluss)
+                    fluss.erwarte(";")
 
-                    konstant[name] = wert
+                    konstantent[name] = wert
+                case wort:
+                    print(f"Unbekannes Hauptwort: `{wort}`")
+                    sys.exit(1)
 
         return kls(prozeduren, konstantent)
 
